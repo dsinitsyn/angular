0491db4b4a897f7229623e4e7bfb1dae89220957
crash <dima@pindrop.nl>
2017-06-04 23:15:47 +0300

    added reactive forms

--- angular-form/src/app/app.component.ts
+++ angular-form/src/app/app.component.ts
@@ -10,6 +10,15 @@ export class AppComponent {
     @ViewChild('f') signupForm: NgForm;
     defaultQuestion = 'pet';
     answer = '';
+    genders = ['male', 'female'];
+    submitted = false;
+    user = {
+        username: '',
+        email: '',
+        secretQuestion: '',
+        answer: '',
+        gender: ''
+    }
 
     constructor(){
         console.log(this.signupForm);
@@ -18,6 +27,21 @@ export class AppComponent {
 
     suggestUserName(){
         const suggestedName = 'Superuser';
+        // this.signupForm.setValue({
+        //     userData: {
+        //         username: suggestedName,
+        //         email: ''
+        //     },
+        //     secret: 'pet',
+        //     questionAnswer: '',
+        //     gender: 'male'
+        // })
+
+        this.signupForm.form.patchValue({
+            userData: {
+                username: suggestedName
+            }
+        })
     }
 
     // onSubmit(form: NgForm) {
@@ -25,6 +49,13 @@ export class AppComponent {
     // }
 
     onSubmit(){
-        console.log(this.signupForm);
+        this.submitted = true;
+        this.user.username = this.signupForm.value.userData.username;
+        this.user.email  = this.signupForm.value.userData.email;
+        this.user.secretQuestion = this.signupForm.value.secret;
+        this.user.answer = this.signupForm.value.questionAnswer;
+        this.user.gender = this.signupForm.value.gender;
+
+        this.signupForm.reset();
     }
 }
